8b588b4e3f11ad12d7f18a5631ffa17ab1aca32c
howard31622 <howard31622@gmail.com>
2018-11-17 23:51:41 +0800

    Add files via upload

--- getData.py
+++ getData.py
@@ -2,8 +2,10 @@
 import  picamera
 import  time
 
-for take in range(0,10):
-	camera = picamera.PiCamera()
-	camera.resolution =  (640,480) 
+camera = picamera.PiCamera()
+camera.resolution =  (640,480) 
+for i in range(0,10):
+
 	time.sleep(2)
-	camera.capture('%s.jpg'%(take))
+	#camera.capture('howard.jpg')
+	camera.capture('howard%s.jpg'%(i))
