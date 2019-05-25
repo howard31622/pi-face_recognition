ae27fa7b360f67b4ed4e326a76332c415ca8f41e
howard31622 <howard31622@gmail.com>
2019-05-26 00:14:20 +0800

    Add files via upload

--- testData.py
+++ testData.py
@@ -11,7 +11,14 @@ sys.path.insert(0, "/home/pi/pi-rc522/ChipReader")
 from pirc522 import RFID
 
 # loading model
-svm = cv2.ml.SVM_load("howard_svm_model.mat")
+#svmginger = cv2.ml.SVM_load("ginger_svm_model.mat")
+svmhoward = cv2.ml.SVM_load("howard_svm_model.mat")
+svmlaio = cv2.ml.SVM_load("laio_svm_model.mat")
+svmoscar = cv2.ml.SVM_load("oscar_svm_model.mat")
+svmtingju = cv2.ml.SVM_load("tingju_svm_model.mat")
+svmxin = cv2.ml.SVM_load("xin_svm_model.mat")
+svmyancheng = cv2.ml.SVM_load("yancheng_svm_model.mat")
+svmyuchen = cv2.ml.SVM_load("yuchen_svm_model.mat")
 
 #RFID default setting
 rdr = RFID()
@@ -29,26 +36,46 @@ camera.framerate = 1
 
 
 
-#x =0
+
 def test():
     while(True):
+        
         #use RFID to enter sign in
         #Request tag
         (error,dataasd) = rdr.request()
         #if not error:        
         print("You can use your card")
         (error,getuid) = rdr.anticoll()    
-        #if x == 0 :
-        if not error:    
+        
+        if not error:
+            
+            useRFIDop = time.time()
             print ("Card read UID: "+str(getuid[0])+","+str(getuid[1])+","+str(getuid[2])+","+str(getuid[3]))
             uid = str(getuid[0])+","+str(getuid[1])+","+str(getuid[2])+","+str(getuid[3])
-#            uid = "zzzz"
+            useRFIDed = time.time()
+            
+            
             #take picture
+            print("taking picture")
+            
+            #preview  the camera for user to see himself
+            cameraUseop = time.time()
+            camera.start_preview()
+            camera.preview.fullscreen = True
             camera.capture('./'+uid+'.jpg')
+            cameraUseed = time.time()
+            cameraUse = cameraUseop-cameraUseed
+            print("cameraUse : ",cameraUse)
+            
+            
             #encoding picture
+            encodingaop = time.time()
             image = cv2.imread('./'+uid+'.jpg')
+            encodingaed = time.time()
+            encodinga = encodingaop - encodingaed
+            print("encodinga : ",encodinga)
             rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-	
+            
             # print("rgb : ",rgb)
             # detect the (x, y)-coordinates of the bounding boxes
             # corresponding to each face in the input image
@@ -57,21 +84,56 @@ def test():
             # compute the facial embedding for the face
             encodings = face_recognition.face_encodings(rgb, boxes)
             #print(encodings)
+            
+            camera.stop_preview()
+            
+            #if camera can't encode any face
+            if encodings == "":
+                print("please try again")
+                break
+            
+            
             #input predict model and output the result
             pt_data = np.array(encodings,dtype='float32')
-            (par1,par2) = svm.predict(pt_data)
+            inputSvm = input("Please intput your name. For example : ginger ,howard ,laio ,oscar ,tingju ,xin ,yancheng ,yuchen : ")
+            if inputSvm == "howard":
+                (par1,par2) = svmhoward.predict(pt_data)
+                isTrue(par2[0][0])
+            elif inputSvm == "laio":
+                (par1,par2) = svmlaio.predict(pt_data)
+                isTrue(par2[0][0])
+            elif inputSvm == "oscar":
+                (par1,par2) = svmoscar.predict(pt_data)
+                isTrue(par2[0][0])
+            elif inputSvm == "tingju":
+                (par1,par2) = svmtingju.predict(pt_data)
+                isTrue(par2[0][0])
+            elif inputSvm == "xin":
+                (par1,par2) = svmxin.predict(pt_data)
+                isTrue(par2[0][0])
+            elif inputSvm == "yancheng":
+                (par1,par2) = svmyancheng.predict(pt_data)
+                isTrue(par2[0][0])
+            elif inputSvm == "yuchen":
+                (par1,par2) = svmyuchen.predict(pt_data)
+                isTrue(par2[0][0])
             #print("the result of prediction  : " )
-            if str(int(par2[0][0])) == "1" :
-                print("")    
-                print("The user is correct")
-                print("")
-            else :
             
-                print("The user is error ")
-            time.sleep(5)
             #print(par2[0][0])
     
     #print("testing")
+
+#to test the predict model is truev
+def isTrue(result):
+    if str(int(result)) == "1" :
+        print("")    
+        print("The user is correct")
+        print("")
+    else :
+            
+        print("The user is error ")
+        time.sleep(3)
+    
     
 if __name__ == "__main__":
     test()
